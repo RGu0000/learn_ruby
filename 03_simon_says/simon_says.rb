1725637ee047e2ed25493a6b769130aed1feb882
RGu0000 <RDInquire>
2018-02-11 13:28:41 +0100

    Exercise 03 done

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,28 @@
-#write your code here
+def echo(text)
+  text
+end
+
+def shout(text)
+  text.upcase
+end
+
+def repeat(text,times=2)
+  result=text
+  (times-1).times do |str|
+    result+=" "+text
+  end
+  result
+end
+
+def start_of_word(text,number)
+  text[0, number]
+end
+
+def first_word(text)
+  text.split.first
+end
+
+def titleize(text)
+  words=%w(end over and the)
+  text.capitalize.gsub(/(\w+)/) {|found| words.include?(found)? found : found.capitalize}
+end
